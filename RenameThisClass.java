755f0f9b9abe3ef087de1a4f8d4094badadfc365
Elizabeth Barefoot <elizabeth.barefoot@mrjaffesclass.com>
2023-09-12 11:33:32 -0700

    Commit message

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -8,6 +8,13 @@
 public class RenameThisClass
 {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int i = 10;
+        float f = 2.5f;
+        f += i;
+        boolean flag = true;
+        flag = !flag; 
+        System.out.println(i++);   
+        System.out.println(f);
     }
 }
+
