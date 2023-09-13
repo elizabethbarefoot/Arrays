35bcf5316ae292968b2b27729d3ca7c5e861677a
Elizabeth Barefoot <elizabeth.barefoot@mrjaffesclass.com>
2023-09-13 11:29:39 -0700

    string

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -13,6 +13,11 @@ public class RenameThisClass
         f += i;
         boolean flag = true;
         flag = !flag; 
+        String string1 = "This is a string";
+        String string2 = "This is a string";
+        string1.equals(string2);
+        String string3 = "I am also a string";
+        boolean stringsequal = string1.equals(string3);
         System.out.println(i++);   
         System.out.println(f);
     }
